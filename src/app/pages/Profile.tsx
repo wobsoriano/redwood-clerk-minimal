9522d12f384181820cc7648e1da996f55fe59e03
wobsoriano <sorianorobertc@gmail.com>
2025-05-19 14:12:54 -0700

    add clerk client usage example

--- src/app/pages/Profile.tsx
+++ src/app/pages/Profile.tsx
@@ -1,10 +1,18 @@
 import { Layout } from "../components/Layout";
 import { UserProfile } from "@/app/components/UserProfile"
 import { UserButton } from "@/app/components/UserButton"
+import { clerkClient } from "@/app/lib/clerkClient";
+import type { RequestInfo } from "rwsdk/worker";
 
-export function Profile() {
+export async function Profile({ ctx }: RequestInfo) {
+  // We have an interruptor and we're sure ctx.auth is available
+  const { userId } = ctx.auth!
+
+  const user = await clerkClient.users.getUser(userId!)
+
   return (
     <Layout>
+      <div>Welcome, { user.fullName }. </div>
       <UserButton />
       <UserProfile />
     </Layout>
